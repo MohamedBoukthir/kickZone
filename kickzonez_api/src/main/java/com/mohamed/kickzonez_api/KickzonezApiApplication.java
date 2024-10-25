dd01d10487c4663bafa4bcd7ead73482376248af
MohamedBoukthir <medboukthir7@gmail.com>
2024-10-25 22:22:12 +0100

    project initialization

---
+++ kickzonez_api/src/main/java/com/mohamed/kickzonez_api/KickzonezApiApplication.java
@@ -0,0 +1,13 @@
+package com.mohamed.kickzonez_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickzonezApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickzonezApiApplication.class, args);
+	}
+
+}
---
+++ kickzonez_api/src/test/java/com/mohamed/kickzonez_api/KickzonezApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohamed.kickzonez_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickzonezApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
