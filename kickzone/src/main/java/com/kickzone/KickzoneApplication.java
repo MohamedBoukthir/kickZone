51eab05c9d9059116e697ab774fc76389712e4f7
MohamedBoukthir <medboukthir7@gmail.com>
2024-10-26 16:07:42 +0100

    project initialization

---
+++ kickzone/src/main/java/com/kickzone/KickzoneApplication.java
@@ -0,0 +1,13 @@
+package com.kickzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickzoneApplication.class, args);
+	}
+
+}
